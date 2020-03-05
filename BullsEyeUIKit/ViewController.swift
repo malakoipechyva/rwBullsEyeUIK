0451cef798bd3cf358a42472dc890d808f444dee
Yauhen  Chyrko <chirko.eugene@gmail.com>
2020-03-05 15:25:39 +0300

    init: start BullsEye

---
+++ BullsEyeUIKit/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  BullsEyeUIKit
+//
+//  Created by malakoipechyva on 3/5/20.
+//  Copyright © 2020 malakoipechyva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  @IBAction func showAlert() {
+    print("Hello")
+  }
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+## BullsEye game from [Ray Wenderlich's courses](https://www.raywenderlich.com/5993-your-first-ios-and-uikit-app), UIKit version
